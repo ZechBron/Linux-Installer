bcc4f33b9103f1615a6b0ab1507e78b8b303bf2b
Zech Bron <39665654+ZechBron@users.noreply.github.com>
2020-10-02 12:52:23 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-zCh="Version 0.3"
-
-check=$(curl https://raw.githubusercontent.com/ZechBron/Linux-Installer/linux-installer-images/updates)
-
-if [ "$zCh" = "$check" ]; then
-   echo -e "\e[92mThe version of this tool is Version 0.3. However, there's Version 0.3 available."
-   echo -e "Do you want to update it (y/n)? \e[0m"
-   read ChB
-   
-   if [ "$ChB" = "y" ]; then
       rm -f linux-installer.sh README.md setup.sh
       rm -rf Linux-Installer
       cd -
@@ -19,12 +9,3 @@ if [ "$zCh" = "$check" ]; then
       cd Linux-Installer
       chmod +x setup.sh
       ./setup.sh
-   elif [ "$ChB" = "n" ]; then
-      exit
-   else
-      echo -e "\e[91mWrong input. Just type small letter y or n\e[0m"
-   fi
-
-else
-   echo -e "\e[92mThere is no latest version.\e[0m"
-fi
